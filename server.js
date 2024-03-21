4b8ffbb14065abac76d79e11fb9790c70e1125cb
okkkko <konopelskaoksana@gmail.com>
2024-03-21 14:56:06 +0200

    Frontend

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+const path = require('path');
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const connectDB = require('./config/db');
@@ -7,16 +9,23 @@ connectDB();
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the RandomIdeas API' });
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const ideaRouter = require('./routes/ideas');
-app.use('/api/ideas', ideaRouter);
+app.use(
+  cors({
+    origin: ['http://localhost:5000', 'http://localhost:3000'],
+    credentials: true,
+  })
+);
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the RandomIdeas API' });
 });
+
+const ideasRouter = require('./routes/ideas');
+app.use('/api/ideas', ideasRouter);
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
